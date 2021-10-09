f6bd4499a6f04bcd4aace8a754a20c7847d6b3b9
andrewSuprun <86914377+andrewSuprun@users.noreply.github.com>
2021-10-09 18:06:01 +0200

    Create day20.js

---
+++ day20.js
@@ -0,0 +1,5 @@
+#39: Regular Expression--""?"", ""*"", ""+"" and ""{}"" https://www.codewars.com/kata/573bca07dffc1aa693000139
+#40: Regular Expression--""|"", ""[]"" and ""()"" http://www.codewars.com/kata/573d11c48b97c0ad970002d4
+
+var regex=/^-?9.*0000$/;
+var regex=/(http:|https:)\/\/[a-z0-9\.]+\.(net|com)/gi
