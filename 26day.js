ac0beec8255a8f65056df75843ddb413fbeea87c
Andrii Suprun <86914377+andrewSuprun@users.noreply.github.com>
2021-10-15 22:46:27 +0200

    Update 26day.js

--- 26day.js
+++ 26day.js
@@ -1,3 +1,8 @@
+Power of bind http://www.codewars.com/kata/power-bind
+Sort with Arrow Functions https://www.codewars.com/kata/sort-with-arrow-functions
 
+Function.prototype.bind = function (ctx1) {
+ return (ctx = ctx1) => this.call(ctx, ctx);
+};
 
 const OrderPeople = (people) => people.sort( (a, b) => a.age - b.age );
