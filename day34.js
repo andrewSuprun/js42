859f9f7f7c103efb162b9900dd372601de8f93df
Andrii Suprun <86914377+andrewSuprun@users.noreply.github.com>
2021-10-16 21:16:16 +0200

    Update day34.js

--- day34.js
+++ day34.js
@@ -1,3 +1,7 @@
+Adam and Eve - https://www.codewars.com/kata/basic-subclasses-adam-and-eve/
+Labrador - https://www.codewars.com/kata/56ff9b53140fcca90b000530/
+Classy Classes - https://www.codewars.com/kata/55a144eff5124e546400005a
+
 class God{
   static create(){
     return [new Man(), new Woman()];
@@ -22,9 +26,23 @@ class Woman extends Human {
   }
 }
 
+//
 
 class Labrador extends Dog {
   constructor(name, age, gender, master) {
     super(name, age, gender, "Labrador", "Large", master, true);
+  }
+}
+
+//
+
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+
+  get info() {
+    return `${this.name}s age is ${this.age}`
   }
 }
