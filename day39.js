b82d73345bd059490328d50169656eeac2a3b237
Andrii Suprun <86914377+andrewSuprun@users.noreply.github.com>
2021-10-28 22:01:48 +0200

    Create day39.js

---
+++ day39.js
@@ -0,0 +1,12 @@
+function* generator(a) {
+  let b = 1;
+  if (a === 0 ) {
+  while (true)
+  yield `0 x ${a++} = 0`
+  }
+  if (a<6){
+  while (true)
+  yield `1 x ${a++} = ${b++}`}
+  while (a < a+1)
+  yield `${a} x ${b++} = ${a*(b-1)}`
+}
